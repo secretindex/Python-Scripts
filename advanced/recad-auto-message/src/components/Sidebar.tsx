1c9ab1df622e8cf0d01d217b4521766790e47e50
secretindex <caio.smelo04@gmail.com>
2024-07-25 15:04:08 -0300

    trying to make menu redirect to other routes with React Router Link

--- advanced/recad-auto-message/src/components/Sidebar.tsx
+++ advanced/recad-auto-message/src/components/Sidebar.tsx
@@ -10,7 +10,8 @@ import ListItemIcon from "@mui/material/ListItemIcon"
 import ListItemText from "@mui/material/ListItemText"
 import InboxIcon from "@mui/icons-material/MoveToInbox"
 import MailIcon from "@mui/icons-material/Mail"
-import MenuIcon from '@mui/icons-material/Menu';
+import MenuIcon from "@mui/icons-material/Menu"
+import { Link } from "react-router-dom"
 
 export default function TemporaryDrawer() {
   const [open, setOpen] = useState(false)
@@ -22,15 +23,17 @@ export default function TemporaryDrawer() {
   const DrawerList = (
     <Box sx={{ width: 250 }} role="presentation" onClick={toggleDrawer(false)}>
       <List>
-        {["Ativos/Inativos", "Aposentados"].map((text, index) => (
-          <ListItem key={text} disablePadding>
-            <ListItemButton>
-              <ListItemIcon>
-                {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-              </ListItemIcon>
-              <ListItemText primary={text} />
-            </ListItemButton>
-          </ListItem>
+        {["Ativos", "Aposentados"].map((text, index) => (
+          <Link to={text}>
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          </Link>
         ))}
       </List>
       <Divider />
