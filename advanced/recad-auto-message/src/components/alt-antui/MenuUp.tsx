213209b513b9361e2bba2474e822330417899b68
secretindex <caio.smelo04@gmail.com>
2024-08-18 02:49:39 -0300

    change menu style

--- advanced/recad-auto-message/src/components/alt-antui/MenuUp.tsx
+++ advanced/recad-auto-message/src/components/alt-antui/MenuUp.tsx
@@ -9,55 +9,18 @@ import { Link } from "react-router-dom"
 
 const { Header } = Layout
 
-// const TopBarMenu = () => {
-//   return (
-//     <>
-//     <Box sx={{ flexGrow: 1 }}>
-//       <AppBar position="static">
-//         <Toolbar>
-//           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-//             Aucad
-//           </Typography>
-//         </Toolbar>
-//       </AppBar>
-//     </Box>
-//     <Layout>
-//       <Header>
-//         <div className="logo" style={{ float: 'left', color: 'white', fontSize: '20px' }}>
-//           MyApp
-//         </div>
-//         <Menu theme="dark" mode="horizontal" defaultSelectedKeys={['1']} style={{ lineHeight: '64px' }}>
-//           <Menu.Item key="1" icon={<HomeOutlined />}>
-//             Home
-//           </Menu.Item>
-//           <Menu.Item key="2" icon={<UserOutlined />}>
-//             Profile
-//           </Menu.Item>
-//           <Menu.Item key="3" icon={<SettingOutlined />}>
-//             Settings
-//           </Menu.Item>
-//         </Menu>
-//       </Header>
-//     </Layout>
-//     </>
-//   );
-// };
-
 const { Title } = Typography
 
 export default function MenuAnt() {
   return (
     <Layout>
-      <Header style={{ padding: 0, margin: 0, height: "100%" }}>
+      <Header className="p-0 m-0 h-full">
         <Menu
           mode="horizontal"
           defaultSelectedKeys={["1"]}
+          className="flex items-center gap-1 px-2 m-0"
           style={{
             lineHeight: "3rem",
-            margin: "0",
-            display: "flex",
-            alignItems: "center",
-            padding: "0 0.7rem",
           }}
         >
           <Title
@@ -67,17 +30,17 @@ export default function MenuAnt() {
             Aucad
           </Title>
           <Menu.Item key="1" icon={<HomeOutlined />}>
-            <Link to="/" style={{ color: "inherit" }}>
+            <Link to="/" className="text-inherit">
               Home
             </Link>
           </Menu.Item>
           <Menu.Item key="2" icon={<UserOutlined />}>
-            <Link to="/aposentados" style={{ color: "inherit" }}>
+            <Link to="/aposentados" className="text-inherit">
               Aposentados
             </Link>
           </Menu.Item>
           <Menu.Item key="3" icon={<SettingOutlined />}>
-            <Link to="/pensionistas" style={{ color: "inherit" }}>
+            <Link to="/pensionistas" className="text-inherit">
               Pensionistas
             </Link>
           </Menu.Item>
